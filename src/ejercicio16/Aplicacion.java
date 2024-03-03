c02abb684f61cd1eeb06e58efde0fff04c8cdf98
Your Name <you@example.com>
2024-03-03 12:24:57 -0500

    Update

---
+++ src/ejercicio16/Aplicacion.java
@@ -0,0 +1,38 @@
+package ejercicio16;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+
+		EquipoDeporte miEquipo = new EquipoDeporte();
+
+		miEquipo.pais="PERU";
+		miEquipo.deporte="Karate";
+		miEquipo.nroIntegrantes = 11;
+		miEquipo.nombre = "Los novatos";
+
+		miEquipo.validarEquipo();
+		
+		// Si el equipo es de la disciplina de Karate
+		// tiene un descuento por hospedaje
+		
+		boolean esKarate = miEquipo.esDisciplinaKarate();
+		
+		if(esKarate) {
+			System.out.println("Tienes descuento de hospedaje");
+		}
+		
+		// Crear un metodo para obtener el mensaje de Bienvenida ,
+		// Si son de Perù debera devolver , "Bienvenidos equipo de [Disciplina]" ,
+		// Si son de USA debera devolver el mensaje "Welcome team of [Disciplina] "
+		// , para los otros casos debe devolver el mensaje "Bienvenidos"
+		
+		
+		String mensaje = miEquipo.obtenerMensajeBienvenida();
+		
+		System.out.println(mensaje);
+		
+	}
+
+
+}
