b3d320c74419a6f2ae79149555156ba64f60e6ac
Your Name <you@example.com>
2024-03-10 10:57:21 -0500

    Update ejercicio 24

--- src/ejercicio24/Aplicacion2.java
+++ src/ejercicio24/Aplicacion2.java
@@ -5,14 +5,41 @@ public class Aplicacion2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		EquipoMovilApple apple = new EquipoMovilApple();
-		EquipoMovilSamsung samsung = new EquipoMovilSamsung();
-		EquipoMovilXiaomi xiaomi = new EquipoMovilXiaomi();
+//		EquipoMovilApple apple = new EquipoMovilApple();
+//		EquipoMovilSamsung samsung = new EquipoMovilSamsung();
+//		EquipoMovilXiaomi xiaomi = new EquipoMovilXiaomi();
 		
-		System.out.println(apple.obtenerInfo());
-		System.out.println(samsung.obtenerInfo());
-		System.out.println(xiaomi.obtenerInfo());
 		
+		
+//		EquipoMovil apple = new EquipoMovilApple();
+//		EquipoMovil samsung = new EquipoMovilSamsung();
+//		EquipoMovil xiaomi = new EquipoMovilXiaomi();
+//				
+//		System.out.println(apple.obtenerInfo());
+//		System.out.println(samsung.obtenerInfo());
+//		System.out.println(xiaomi.obtenerInfo());
+		
+
+//		EquipoMovil em;
+//		
+//		em = new EquipoMovilApple();
+//		System.out.println(em.obtenerInfo());
+//		
+//		em = new EquipoMovilSamsung();
+//		System.out.println(em.obtenerInfo());
+//		
+//		em = new EquipoMovilXiaomi();
+//		System.out.println(em.obtenerInfo());
+		
+
+		
+		EquipoMovil em[] = {new EquipoMovilApple(),
+							new EquipoMovilSamsung(),
+							new EquipoMovilXiaomi()};
+		
+		for (int i = 0; i < em.length; i++) {
+			System.out.println(em[i].obtenerInfo());			
+		}
 		
 	}
 
