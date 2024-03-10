13ab2b1819ce8efa23e74cc337a057f2b6a6f0dd
Your Name <you@example.com>
2024-03-10 09:19:47 -0500

    Update ejercicio 21

---
+++ src/ejercicio21/Aplicacion.java
@@ -0,0 +1,16 @@
+package ejercicio21;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		// 
+		
+		EquipoMovilApple ema =  new EquipoMovilApple();
+		
+		String info = ema.obtenerInfo();
+		
+		System.out.println(info);
+		
+	}
+
+}
--- src/ejercicio21/EquipoMovilApple.java
+++ src/ejercicio21/EquipoMovilApple.java
@@ -2,4 +2,9 @@ package ejercicio21;
 
 public class EquipoMovilApple extends EquipoMovil {
 
+	@Override
+	public String obtenerInfo() {
+		return "Equipo Apple";
+	}
+	
 }
