46031f1c03b21019d42d9fe145f88019eba05d67
Your Name <you@example.com>
2024-03-10 10:42:33 -0500

    Update ejercicio 24

--- src/ejercicio24/Aplicacion2.java
+++ src/ejercicio24/Aplicacion2.java
@@ -4,7 +4,16 @@ public class Aplicacion2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		EquipoMovilApple apple = new EquipoMovilApple();
+		EquipoMovilSamsung samsung = new EquipoMovilSamsung();
+		EquipoMovilXiaomi xiaomi = new EquipoMovilXiaomi();
+		
+		System.out.println(apple.obtenerInfo());
+		System.out.println(samsung.obtenerInfo());
+		System.out.println(xiaomi.obtenerInfo());
+		
+		
 	}
 
 }
