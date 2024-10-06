9446ee67ac9cf0fea31754022b6ce136aa9e3b9c
jgomezz <jgomezz@gmail.com>
2024-10-06 08:16:31 -0500

    Update

---
+++ src/pe/edu/tecsup/poo/_05/sobrecarga/Aplicacion.java
@@ -0,0 +1,21 @@
+package pe.edu.tecsup.poo._05.sobrecarga;
+
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+		OperacionMatematica m = new OperacionMatematica();
+
+		int a = 10;
+		int b = 3;		
+		int c = 13;	
+
+		System.out.println("La suma es " + m.multiplicar(a, b));
+		System.out.println("La suma es " +  m.multiplicar(a, b, c));
+		System.out.println("La suma es " + m.multiplicar(1,2,3,4,5,6));
+		
+	}
+
+}
