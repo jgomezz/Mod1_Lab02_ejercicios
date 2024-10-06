870c3f6634c96f8c19848793eb8150025e25a252
jgomezz <jgomezz@gmail.com>
2024-10-06 09:05:37 -0500

    update

--- src/pe/edu/tecsup/poo/_07/estatico/Aplicacion.java
+++ src/pe/edu/tecsup/poo/_07/estatico/Aplicacion.java
@@ -1,7 +1,21 @@
 package pe.edu.tecsup.poo._07.estatico;
 
-
+/*
+ * 
+ * 
+ */
 public class Aplicacion {
+	
+	// Crear la clase Bienvenida
+    // 
+    // Crear el método estático saludar que envie un mensaje por pantalla
+    // en castellano o ingles , dependiento del tipo de lenguaje del argumento
+    // que se le pasa . 
+    //
+    //    Si se le pasa la cadena es "ES" muestra el mensaje "Buenos dias"
+    //    Si se le pasa la cadena es "EN" muestra el mensaje "Good Morning"
+	//
+	// En la clase Aplicacion., deberá invocar al metodo
 
 	public static void main(String[] args) {
       
