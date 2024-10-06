153540b8b7bcabb8399065d833f8c3ade869f94c
jgomezz <jgomezz@gmail.com>
2024-10-06 10:14:34 -0500

    update

--- src/pe/edu/tecsup/poo/_09/herencia/Aplicacion.java
+++ src/pe/edu/tecsup/poo/_09/herencia/Aplicacion.java
@@ -5,14 +5,23 @@ public class Aplicacion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		EquipoMovilXiaomi xiaomi 
-			= new EquipoMovilXiaomi();
+		EquipoMovilXiaomi xiaomi = new EquipoMovilXiaomi();
 		
 		xiaomi.setMarca("Xiaomi");
 		xiaomi.setModelo("Redmi 12");
 		xiaomi.setAnho(2023);
 		
 		System.out.println(xiaomi.obtenerInfo());
+
+		EquipoMovilApple apple = new EquipoMovilApple();
+		
+		apple.setMarca("Apple");
+		apple.setModelo("iPhone 14c");
+		apple.setAnho(2022);
+		
+		System.out.println(apple.obtenerInfo());
+
+	
 	}
 
 }
--- src/pe/edu/tecsup/poo/_09/herencia/EquipoMovilApple.java
+++ src/pe/edu/tecsup/poo/_09/herencia/EquipoMovilApple.java
@@ -2,4 +2,11 @@ package pe.edu.tecsup.poo._09.herencia;
 
 public class EquipoMovilApple extends EquipoMovil {
 
+	@Override
+	public String obtenerInfo() {
+		return this.modelo;
+	}
+
+	
+	
 }
